a8aaecb5e78d318c9212241daddc929e3c304a16
Guillaume Galeazzi <24816508+gzzi@users.noreply.github.com>
2024-04-29 08:34:56 +0200

    cross file

--- main.cpp
+++ main.cpp
@@ -3,9 +3,7 @@
 #include "bad_code.hpp"
 
 int main(void) {
-    int num = 10;
-    int den = 0;
-    int result = bad_div(num, den);
+    int result = bad_div(42, 0);
 
     std::cout << result << std::endl;  // dummy usage
     return 0;
