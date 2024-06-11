0b3335b922c11bdec8dccc1d38463c923517aa42
Guillaume Galeazzi <24816508+gzzi@users.noreply.github.com>
2024-06-11 16:15:06 +0200

    let time to server to start

--- codecheck.sh
+++ codecheck.sh
@@ -3,4 +3,5 @@
 CodeChecker analyze ./compile_commands.json --output ./reports --enable sensitive --ctu
 CodeChecker parse -e html ./reports/ -o ./reports_html
 CodeChecker server &
+sleep 2
 CodeChecker store ./reports -n static_analysis_test
